6d1041c15a1acfd17155e50da8cb12f536908c24
GulfarazHassan <gulfarazhassan535@gmail.com>
2022-12-27 17:45:51 -0500

    change base url in react app

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5001' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://my-admin-dashboard.onrender.com' }),
     reducerPath: "adminApi",
     tagTypes: [
         "User",
